b267e953e01e4b401201edd4c2f552b435340730
Assyarul <muhdassyarul@gmail.com>
2021-06-16 05:22:04 +0800

    Added modal window to card.

--- screens/SwiperScreen.js
+++ screens/SwiperScreen.js
@@ -1,23 +1,35 @@
 //Code modified from https://blog.logrocket.com/how-to-make-tinder-like-card-animations-with-react-native/
-
+//Modal implementation modified from https://reactnavigation.org/docs/modal/
 import React, {useRef,useState} from 'react';
 import Swiper from 'react-native-deck-swiper'
 import Card from '../components/Card'
-import { Text, View } from 'react-native';
+import { Text, View, Modal, TouchableOpacity, StyleSheet } from 'react-native';
+import SAMPLE_ADVISORS from '../assets/dataAdvisors';
 
 export default function SwiperScreen({navigation}) {
-
-  //For now fixed number of sample cards to show functionality
-  const SAMPLE_CARDS = [
-    {id:"0", name:"James Tan", image: require('../assets/user0.jpg')},
-    {id:"1", name:"Nicholas Lee", image: require('../assets/user1.jpg')},
-  ]
   
   const useSwiper = useRef(null).current
   const handleOnLeft = () => useSwiper.swipeLeft()
   const handleOnRight = () => useSwiper.swipeRight()
   
   const [isDone, setDone] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function renderModal(){
+    return (
+    <View style={styles.centeredView}>
+      <View style={styles.modalView}>
+        <Text style={styles.modalText}>Hello World!</Text>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonClose]}
+          onPress={() => setModalVisible(!modalVisible)}
+        >
+          <Text style={styles.textStyle}>Hide Modal</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    )
+  }
 
   function renderSwiper() {
     if (isDone) {
@@ -28,22 +40,79 @@ export default function SwiperScreen({navigation}) {
       )
     } else {
       return (
-      <Swiper
-        ref ={useSwiper}
-        animateCardOpacity
-        cards = {SAMPLE_CARDS}
-        renderCard={card => <Card card={card}/>}
-        cardIndex={0}
-        stackSize={SAMPLE_CARDS.length}
-        showSecondCard
-        onSwipedAll={()=>{setDone(true)}}
-        backgroundColor="white"
-      />
+      <View style={{flex: 1}}>
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+        >
+          {renderModal()}
+        </Modal>
+        <Swiper
+          ref ={useSwiper}
+          animateCardOpacity
+          cards = {SAMPLE_ADVISORS}
+          renderCard={card => <Card card={card}/>}
+          cardIndex={0}
+          stackSize={SAMPLE_ADVISORS.length}
+          showSecondCard
+          onSwipedAll={()=>{setDone(true)}}
+          backgroundColor="white"
+          onTapCard = {()=> setModalVisible(true)}
+        ></Swiper>
+      </View>
       )
     }
   }
   
-  return (
-    renderSwiper()
-  );
+  return (renderSwiper())
+      
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
